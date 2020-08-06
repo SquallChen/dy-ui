99dcfae17a811cd6ad084c649ef817acec8118c7
dylan <442947365@qq.com>
2020-08-06 16:46:57 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="topnav">
-      <div class="logo" @click="tooggleMenu">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+    <span class="toggleAside" @click="tooggleMenu"></span>
+  </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue'
+import { inject, Ref } from "vue";
 export default {
-  setup(){
-    const menuVisible = inject<Ref<boolean>>('menuVisible')
-    const tooggleMenu = ()=>{
-      menuVisible.value = !menuVisible.value
-    }
-    return {tooggleMenu}
-  }
-}
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    const tooggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return { tooggleMenu };
+  },
+};
 </script>
 <style lang="scss" scoped>
 .topnav {
@@ -26,6 +27,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -37,6 +40,26 @@ export default {
     > li {
       margin: 0 1em;
     }
+  }
+  > .toggleAside {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    >.toggleAside{display: inline-block;}
   }
 }
 </style>
