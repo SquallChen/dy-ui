605601250e66b4bd644b65642803eb11e356134a
dylan <442947365@qq.com>
2020-08-10 11:27:16 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,10 @@
   </div>
   <h1>示例1</h1>
   <div>
-    <Button><slot/></Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 <script lang="ts">
--- src/lib/button.vue
+++ src/lib/button.vue
@@ -1,5 +1,21 @@
 <template>
-  <button>
-    按钮
+  <!-- $attrs默认获取上层传递的所有方法 -->
+  <button v-bind="$attrs" :class="`theme-${theme}`">
+    <slot/>
   </button>
 </template>
+<script lang="ts">
+export default {
+  props:{
+    theme:{
+      type:String,
+      default:'button'
+    },
+  },
+  setup(props,context){
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
