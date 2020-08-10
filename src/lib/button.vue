da572af93c480c480c049e067dab9262ec2dca14
dylan <442947365@qq.com>
2020-08-10 14:34:34 +0800

    支持loading

--- src/lib/button.vue
+++ src/lib/button.vue
@@ -1,5 +1,6 @@
 <template>
-  <button class="dy-button" :class="classes" :class="classes" :disabled="disabled">
+  <button class="dy-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="dy-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -20,6 +21,10 @@ export default {
       default: "normal",
     },
     disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     },
@@ -175,6 +180,25 @@ $grey: grey;
         cursor: not-allowed;
         color: $grey;
       }
+    }
+    > .dy-loadingIndicator {
+      width: 14px;
+      height: 14px;
+      display: inline-block;
+      margin-right: 4px;
+      border-radius: 8px;
+      border-color: $blue $blue $blue transparent;
+      border-style: solid;
+      border-width: 2px;
+      animation: dy-spin 1s infinite linear;
+    }
+  }
+  @keyframes dy-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
     }
   }
 }
