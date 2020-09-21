26453a17ce50337a9de256d2a405bbdd8bc97638
dylan <442947365@qq.com>
2020-09-21 21:25:50 +0800

    添加css样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,26 +1,56 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
+  <div class="dy-tabs">
+    <div class="dy-tabs-nav">
+      <div class="dy-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="dy-tabs-content">
+      <component class="dy-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
   </div>
 </template>
 <script lang="ts">
-import Tab from "./Tab.vue"
+import Tab from "./Tab.vue";
 export default {
   setup(props, context) {
-    const defaults = context.slots.default()
+    const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error("Tabs 子标签必须是 Tab");
       }
-    })
-    const titles = defaults.map((tag)=>{
-      return tag.props.title
-    })
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
     return {
       defaults,
-      titles
+      titles,
+    };
+  },
+};
+</script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.dy-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
     }
   }
+  &-content {
+    padding: 8px 0;
+  }
 }
-</script>
+</style>
