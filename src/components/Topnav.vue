62002bcc30d319cd1131b4b3cbafb7aed60a56d5
dylan <442947365@qq.com>
2020-09-26 16:29:38 +0800

    添加图标和背景色

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-king"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -21,8 +25,9 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
-  background: pink;
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -36,6 +41,10 @@ export default {
     max-width: 6em;
     margin-right: auto;
   }
+  >svg {
+      width: 32px;
+      height: 32px;
+    }
   > .menu {
     display: flex;
     white-space: nowrap;
@@ -62,7 +71,9 @@ export default {
     > .logo {
       margin: 0 auto;
     }
-    >.toggleAside{display: inline-block;}
+    > .toggleAside {
+      display: inline-block;
+    }
   }
 }
 </style>
